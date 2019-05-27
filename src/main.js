a3f8b6cf567bde84e84fdb3386d1bfd231a8cbcb
Praveen Puglia <praveenpuglia@gmail.com>
2019-05-27 19:33:05 +0530

    feat: copy button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// Lego blocks setup
+import { BCopyPlugin } from "@vue-lego/blocks";
+Vue.use(BCopyPlugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
